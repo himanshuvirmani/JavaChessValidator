b8f2be6ba70ca2436fcb27db5c0080124da18e94
Himanshu Virmani <himanshu.virmani@PW-C02Q1192G8WN.local>
2017-09-24 10:08:21 +0530

    improved tests

--- src/test/java/com/himanshu/controller/TestGameController.java
+++ src/test/java/com/himanshu/controller/TestGameController.java
@@ -12,26 +12,23 @@ public class TestGameController {
 
     private static final String BOARD_START = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-    private FenChessPosition fenChessPosition;
-
     @Before
     public void setUpBoard() {
-        fenChessPosition = new FenChessPosition(BOARD_START);
     }
 
     @Test
     public void testMove() {
 //        String[] history = {"f3", "e5", "g4", "Qh4"};
-        String[] history = {"e4","c5"};
+        final String[] history = {"e4","c5"};
         ChessRules chessRules = new ChessRulesImpl();
         ChessPosition position = chessRules.getInitialPosition();
         AnMoveInterpreter interpreter = new AnMoveInterpreterImpl(chessRules);
         for (String pgnMove : history) {
             ChessMovePath path = interpreter.convertAnToMove(position, pgnMove);
             position = ChessHelper.applyMoveAndSwitch(chessRules, position, path);
+            BoardInterpreter boardInterpreter = new FenInterpreterImpl();
+            System.out.println(boardInterpreter.convertPositionToString(position));
         }
-        BoardInterpreter boardInterpreter = new FenInterpreterImpl();
-        System.out.println(boardInterpreter.convertPositionToString(position));
 
     }
 
